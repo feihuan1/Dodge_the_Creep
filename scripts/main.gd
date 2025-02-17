4edde5c0ebdc193b06b9db159f9fb6d520173e9b
Feihuan Peng <ericrealtor1997@gmail.com>
2025-02-16 19:24:22 -0800

    fix bug

--- scripts/main.gd
+++ scripts/main.gd
@@ -26,20 +26,22 @@ func new_game():
 	$Music.play()
 
 func _on_mob_timer_timeout() -> void:
-	var mob = mob_scene.instantiate()
 	
 	var mob_spawn_location = $MobPath/MobSpawnPosition
 	mob_spawn_location.progress_ratio = randf()
 	
+	var mob = mob_scene.instantiate()
+	
 	var direction = mob_spawn_location.rotation + PI/2
+	add_child(mob)
 	
 	mob.position = mob_spawn_location.position
 	
 	direction += randf_range(-PI/4, PI/4)
+	mob.rotation = direction
 	
 	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
 	mob.linear_velocity = velocity.rotated(direction)
-	$Mobs.add_child(mob)
 
 func _on_score_timer_timeout() -> void:
 	score += 1
